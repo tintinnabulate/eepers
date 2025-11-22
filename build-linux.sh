312c15729650008295bc61257ccf3cd120cca217
tintinnabulate <tintinn@bulate>
2025-11-22 20:00:48 +0000

    Allocate_Eeper is difficult to get into Ada2012
    Revert to 2022 for now

--- build-linux.sh
+++ build-linux.sh
@@ -2,7 +2,7 @@
 
 set -xe
 
-gnatmake -f -O3 -Wall -Wextra -gnat2012 -o eepers-linux eepers.adb -bargs -static -largs -L./raylib/raylib-5.0_linux_amd64/lib/ -l:libraylib.a -lm  -pthread
+gnatmake -f -O3 -Wall -Wextra -gnat2022 -o eepers-linux eepers.adb -bargs -static -largs -L./raylib/raylib-5.0_linux_amd64/lib/ -l:libraylib.a -lm  -pthread
 ./eepers-linux
 
 # gnatmake -f -Wall -Wextra -gnat2022 test.adb -largs -L./raylib/raylib-5.0_linux_amd64/lib/ -l:libraylib.a -lm
